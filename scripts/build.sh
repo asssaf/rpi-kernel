e27150c6cc78c26f62893891f16275f6fd123d9b
assaf <445552+asssaf@users.noreply.github.com>
2026-07-14 23:37:27 -0700

    make build.sh more configurable

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+: ${ARCH:=arm64}
+: ${CROSS_COMPILE:=/bin/aarch64-linux-gnu-}
+: ${KERNEL_VERSION:=6.18.38}
+: ${DEFCONFIG:=bcm2712_defconfig}
+: ${TARGET=kernel}
+: ${OUTPUT_TAR:=""}
+: ${OUTPUT_LOCAL:=""}
+
 docker build \
-	--build-arg ARCH=arm64 \
-	--build-arg CROSS_COMPILE=/bin/aarch64-linux-gnu- \
-	--build-arg KERNEL_VERSION=6.12.25 \
+	--build-arg "ARCH=${ARCH}" \
+	--build-arg "CROSS_COMPILE=${CROSS_COMPILE}" \
+	--build-arg "KERNEL_VERSION=${KERNEL_VERSION}" \
+	--build-arg "DEFCONFIG=${DEFCONFIG}" \
 	-f docker/Dockerfile \
-	--target=kernel \
-	--output=type=local,dest=/tmp/out \
+	--target "${TARGET}" \
+        ${OUTPUT_TAR:+--output "type=tar,dest=$OUTPUT_TAR"} \
+        ${OUTPUT_LOCAL:+--output "type=local,dest=$OUTPUT_LOCAL"} \
 	.
