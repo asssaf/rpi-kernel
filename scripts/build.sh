9cd05db51b9fb617fe75aad7cb0abb60231faee8
assaf <445552+asssaf@users.noreply.github.com>
2026-07-17 16:24:11 -0700

    add test stage

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@
 : ${KERNEL_VERSION:=6.18.38}
 : ${DEFCONFIG:=bcm2711_defconfig}
 : ${TARGET=kernel}
+: ${KERNEL_STAGE:=""}
+: ${MODULES_STAGE:=""}
 : ${OUTPUT_TAR:=""}
 : ${OUTPUT_LOCAL:=""}
 
@@ -13,8 +15,11 @@ docker build \
 	--build-arg "CROSS_COMPILE=${CROSS_COMPILE}" \
 	--build-arg "KERNEL_VERSION=${KERNEL_VERSION}" \
 	--build-arg "DEFCONFIG=${DEFCONFIG}" \
+	${KERNEL_STAGE:+--build-arg "KERNEL_STAGE=${KERNEL_STAGE}"} \
+	${MODULES_STAGE:+--build-arg "MODULES_STAGE=${MODULES_STAGE}"} \
 	-f docker/Dockerfile \
 	--target "${TARGET}" \
         ${OUTPUT_TAR:+--output "type=tar,dest=$OUTPUT_TAR"} \
         ${OUTPUT_LOCAL:+--output "type=local,dest=$OUTPUT_LOCAL"} \
+	--progress=plain \
 	.
